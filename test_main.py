e23430990c0ea8017b982eba70f73925e5465b99
1k-ct <59008019+1k-ct@users.noreply.github.com>
2021-08-25 07:24:45 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -19,9 +19,9 @@ CSV_URL = "https://www.city.kobe.lg.jp/documents/36421/kansensyazokusei.csv"
 file_name = "data/36421-kansensyazokusei.json"
 main.load_script(csv_url=CSV_URL, file_name=file_name)
 
-# args = sys.argv
-# base_url = args[1]
-base_url = "https://api-cache.vaccines.sciseed.jp/public/281000/"
+args = sys.argv
+base_url = args[1]
+
 
 urls = [
     base_url + "articles/",
@@ -44,37 +44,37 @@ file_names = [
     "data/reservation_frame2.json"
 ]
 
-url = base_url + "articles/"
-file_name = "data/articles.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "articles/"
+# file_name = "data/articles.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "department/"
-file_name = "data/department.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "department/"
+# file_name = "data/department.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "available_department/"
-file_name = "data/available_department.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "available_department/"
+# file_name = "data/available_department.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "item/"
-file_name = "data/item.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "item/"
+# file_name = "data/item.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=8"
-file_name = "data/month-8-all.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=8"
+# file_name = "data/month-8-all.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=9"
-file_name = "data/month-9-all.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=9"
+# file_name = "data/month-9-all.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "reservation_frame/?department_id=8769&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
-file_name = "data/reservation_frame1.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "reservation_frame/?department_id=8769&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
+# file_name = "data/reservation_frame1.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "reservation_frame/?department_id=8770&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
-file_name = "data/reservation_frame2.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "reservation_frame/?department_id=8770&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
+# file_name = "data/reservation_frame2.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
 # for i in len(urls):
 #     main.fetch_load_json(URL=urls[i], file_path=file_names[i])
