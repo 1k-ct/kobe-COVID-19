8a4d1ad26b4419201e90871b5e84a9e651a6f459
1k-ct <59008019+1k-ct@users.noreply.github.com>
2021-08-27 02:36:21 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -22,60 +22,60 @@ main.load_script(csv_url=CSV_URL, file_name=file_name)
 # args = sys.argv
 # base_url = args[1]
 
-base_url = "https://api-cache.vaccines.sciseed.jp/public/281000/"
+# base_url = "https://api-cache.vaccines.sciseed.jp/public/281000/"
 
-urls = [
-    base_url + "articles/",
-    base_url + "department/",
-    base_url + "available_department/",
-    base_url + "item/",
-    base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=8",
-    base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=9",
-    base_url + "reservation_frame/?department_id=8769&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10",
-    base_url + "reservation_frame/?department_id=8770&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
-]
-file_names = [
-    "data/articles.json",
-    "data/department.json",
-    "data/available_department.json",
-    "data/item.json",
-    "data/month-8-all.json",
-    "data/month-9-all.json",
-    "data/reservation_frame1.json",
-    "data/reservation_frame2.json"
-]
+# urls = [
+#     base_url + "articles/",
+#     base_url + "department/",
+#     base_url + "available_department/",
+#     base_url + "item/",
+#     base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=8",
+#     base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=9",
+#     base_url + "reservation_frame/?department_id=8769&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10",
+#     base_url + "reservation_frame/?department_id=8770&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
+# ]
+# file_names = [
+#     "data/articles.json",
+#     "data/department.json",
+#     "data/available_department.json",
+#     "data/item.json",
+#     "data/month-8-all.json",
+#     "data/month-9-all.json",
+#     "data/reservation_frame1.json",
+#     "data/reservation_frame2.json"
+# ]
 
-url = base_url + "articles/"
-file_name = "data/articles.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "articles/"
+# file_name = "data/articles.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "department/"
-file_name = "data/department.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "department/"
+# file_name = "data/department.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "available_department/"
-file_name = "data/available_department.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "available_department/"
+# file_name = "data/available_department.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "item/"
-file_name = "data/item.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "item/"
+# file_name = "data/item.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=8"
-file_name = "data/month-8-all.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=8"
+# file_name = "data/month-8-all.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=9"
-file_name = "data/month-9-all.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "available_date/?department_id=8769&item_id=3&year=2021&month=9"
+# file_name = "data/month-9-all.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "reservation_frame/?department_id=8769&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
-file_name = "data/reservation_frame1.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "reservation_frame/?department_id=8769&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
+# file_name = "data/reservation_frame1.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
-url = base_url + "reservation_frame/?department_id=8770&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
-file_name = "data/reservation_frame2.json"
-main.fetch_load_json(URL=url, file_path=file_name)
+# url = base_url + "reservation_frame/?department_id=8770&item_id=3&start_date_after=2021-07-14&start_date_before=2022-10-10"
+# file_name = "data/reservation_frame2.json"
+# main.fetch_load_json(URL=url, file_path=file_name)
 
 
 
